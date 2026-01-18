6de2d097ff1e675c6df8afc82267b1f7894b5983
Temo <tdzhgarkava@yahoo.com>
2026-01-18 20:06:37 +0400

    js

---
+++ app.js
@@ -0,0 +1,7 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
