d9bd6b82335181ada23a2f79e266efedfcd61f4a
Temo <tdzhgarkava@yahoo.com>
2026-01-18 20:09:13 +0400

    js update functions

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const countLabel = document.getElementById("countLabel");
 
 let count = 0;
 
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count
+}
